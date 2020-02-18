73eff4215ca1cc64fe9b92dc1305e54427b2ba2e
it3 <it3@pitertour.ru>
2020-02-18 19:05:00 +0300

    events

--- src/store/modules/state.js
+++ src/store/modules/state.js
@@ -2,8 +2,7 @@ import { stateKeys } from './index'
 
 const state = {
   loading: true,
-  isProjectsShow: false,
-  isEventsShow: true,
+  isProjectsShow: true,
   userDataPath: '',
   downloadsTargetPath: '',
   md: '',
