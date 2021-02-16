48f93037caacc99c9546213b8f157b2dd89f8e1c
Dmitriy Nevezhin <nevezhin@lanit.ru>
2021-02-16 14:47:29 +0300

    test app

--- test/App.spec.ts
+++ test/App.spec.ts
@@ -2,19 +2,20 @@ import { createLocalVue, mount } from '@vue/test-utils'
 import 'reflect-metadata'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import vueElectron from 'vue-electron'
-import App from '../src/App';
+import App from '../src/App'
 import store from '../src/store'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
-localVue.use(vueElectron)
+
+Vue.prototype.$electron = require('electron')
 
 describe('App', () => {
   it('App is correctly rendered', async () => {
     let options = { sync: false, store, localVue }
     const wrapper = mount(App, options);
     await Vue.nextTick()
-    expect(wrapper.text()).toEqual('A')
+    expect(wrapper.vm.$el.id)
+      .toEqual('app')
   })
 })
