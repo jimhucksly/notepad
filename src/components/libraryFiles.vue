18071b77f3cab83522ddb0d163bebb7ac0a03cd8
Dmitriy Nevezhin <nevezhin@lanit.ru>
2021-04-23 10:35:11 +0300

    root colors

--- src/components/libraryFiles.vue
+++ src/components/libraryFiles.vue
@@ -5,7 +5,20 @@
       expanded: init
     }"
   >
-    AAAAAAAAAAa
+    <div class="library_files_title">Library: Files</div>
+    <div class="library_files_inner">
+      <ul>
+        <li>
+          <span>SOLID</span>
+        </li>
+        <li>
+          <span>Алгоритмы</span>
+        </li>
+        <li>
+          <span>Паттерны</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script src="./libraryFiles.ts" lang="ts"></script>
