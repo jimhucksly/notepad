c55a5d3b3d393575461ba810eed960d7e8b10106
jimhucksly <jimhucksly@gmail.com>
2023-02-04 21:34:56 +0300

    nodemailer

--- api/routes.post.js
+++ api/routes.post.js
@@ -1,5 +1,4 @@
-const axios = require('axios')
-const { endpoint } = require('./config.json')
+const nodemailer = require('nodemailer')
 
 function post(router, db) {
   router.post('/signup', async (req, res, next) => {
@@ -7,25 +6,39 @@ function post(router, db) {
       const { login, pass, name, email } = req.body
       if (login && pass && name && email) {
         // await db.command().user({ login, pass, name, email }).signup()
-        const body = { hello: 'world' };
-        const { data } = await axios.post(
-          endpoint + '/sendmail.php',
-          body,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Honeypot': 'notepad_app_api'
-            }
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.beget.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: 'noreply@dn-web.ru',
+            pass: 'vGqr3QX@vASKmm5~',
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+        transporter.verify(function (error, success) {
+          if (error) {
+            console.log(error);
+          } else {
+            const message = {
+              from: 'noreply@dn-web.ru',
+              to: email,
+              subject: 'Message title !!!!!!',
+              text: 'Plaintext version of the message',
+              html: '<p>HTML version of the message</p>'
+            };
+            transporter.sendMail(message, function(err) {
+              console.log(err)
+            })
           }
-        )
+        });
         res.send({
           status: 'success',
-          message: data
+          message: 'user created'
         })
-        // res.send({
-        //   status: 'success',
-        //   message: 'user created'
-        // })
+        return
       }
       throw new Error('bad request')
     } catch (e) {
